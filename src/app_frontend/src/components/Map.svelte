e70f0aedee478f841782aef796320b6d5c4ffb41
Bolek121 <115473541+Bolek121@users.noreply.github.com>
2024-07-24 14:17:55 +0200

    map integrated, import fix(moved map module from root modules to frontapp modules)

--- src/app_frontend/src/components/Map.svelte
+++ src/app_frontend/src/components/Map.svelte
@@ -1,16 +1,20 @@
 <script>
-    import { MapLibre } from 'svelte-maplibre';
-  </script>
-  
+  import { MapLibre } from 'svelte-maplibre';
+</script>
+
+<div>
+  <div>MapLibre</div>
   <MapLibre 
-    center={[50,20]}
-    zoom={7}
-    class="map"
-    standardControls
-    style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json" />
-  
-  <style>
-    :global(.map) {
-      height: 500px;
-    }
-  </style>
+  center={[50,20]}
+  zoom={7}
+  class="map"
+  standardControls
+  style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json" />
+</div>
+
+
+<style>
+  :global(.map) {
+    height: 500px;
+  }
+</style>
--- src/app_frontend/src/routes/+page.svelte
+++ src/app_frontend/src/routes/+page.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <main>
-  <Map/>
+  <Map />
 
   <!-- {#if $auth.isReady}
     {#if $auth.isAuthenticated}
