eca09162473dfa6d1f034ff17db1fcc2b35a9dc6
Bolek121 <115473541+Bolek121@users.noreply.github.com>
2024-07-24 11:33:08 +0200

    simple map example

--- src/app_frontend/src/routes/+page.svelte
+++ src/app_frontend/src/routes/+page.svelte
@@ -1,20 +1,23 @@
 <script>
-  import LoggedIn from "../components/LoggedIn.svelte";
-  import LoggedOut from "../components/LoggedOut.svelte";
-  import { auth } from "../lib/auth";
-  import { onMount } from "svelte";
+  import {Map} from "../components/Map.svelte";
+  // import LoggedIn from "../components/LoggedIn.svelte";
+  // import LoggedOut from "../components/LoggedOut.svelte";
+  // import { auth } from "../lib/auth";
+  // import { onMount } from "svelte";
 
-  onMount(() => {
-    $auth.init();
-  });
+  // onMount(() => {
+  //   $auth.init();
+  // });
 </script>
 
 <main>
-  {#if $auth.isReady}
+  <Map/>
+  
+  <!-- {#if $auth.isReady}
     {#if $auth.isAuthenticated}
       <LoggedIn />
     {:else}
       <LoggedOut />
     {/if}
-  {/if}
+  {/if} -->
 </main>
