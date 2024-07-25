91b013ff080f99878ded4ce4d708a15797e4125b
Bolek121 <115473541+Bolek121@users.noreply.github.com>
2024-07-25 03:22:08 +0200

    To-do's

--- src/app_frontend/src/routes/+page.svelte
+++ src/app_frontend/src/routes/+page.svelte
@@ -1,23 +1,44 @@
 <script>
   import Map from "../components/Map.svelte";
-  // import LoggedIn from "../components/LoggedIn.svelte";
-  // import LoggedOut from "../components/LoggedOut.svelte";
-  // import { auth } from "../lib/auth";
-  // import { onMount } from "svelte";
+  import LoggedIn from "../components/LoggedIn.svelte";
+  import LoggedOut from "../components/LoggedOut.svelte";
+  import { auth } from "../lib/auth";
+  import { onMount } from "svelte";
 
-  // onMount(() => {
-  //   $auth.init();
-  // });
+  onMount(() => {
+    $auth.init();
+  });
 </script>
 
 <main>
-  <Map />
+  <div>
+    {#if $auth.isReady}
+      {#if $auth.isAuthenticated} 
+        <div>
+          <!-- 
+          lista komponentów na pasku narzędzi/opcji. Do zrobienia poprzez Routes?
+          przejście do każdego routa albo spowoduje pojawienie się okienka elementu albo  stworzymy całkiem nowy widok, aczkolwiek 1 opcja efektowniejsza
+          
 
-  <!-- {#if $auth.isReady}
-    {#if $auth.isAuthenticated}
-      <LoggedIn />
-    {:else}
-      <LoggedOut />
+          mapa - root - przejście do głównego widoku
+          your events
+          create event
+          settings
+          profile - będziemy chcieli żeby zawierał w środku component LoggedIn
+          searchbar
+          online events 
+          -->
+          <LoggedIn />
+        </div>
+        <div>
+          <Map />
+        </div>
+      {:else}
+        <LoggedOut />
+      {/if}
     {/if}
-  {/if} -->
+  </div>
+  <div>
+    <!-- odnośnik do projektu - link do githuba? -->
+  </div>
 </main>
