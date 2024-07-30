4afb0cda47e62d271353b0ff230cf2996cfef613
karolko9 <bolekbolek909@gmail.com>
2024-07-30 13:46:09 +0200

    profile fixes

--- src/app_frontend/src/routes/+page.svelte
+++ src/app_frontend/src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
   import Map from "../components/Map.svelte";
-  import Profile from "../components/Profile.svelte";
   import LoggedOut from "../components/LoggedOut.svelte";
   import { auth } from "../lib/auth";
   import { onMount } from "svelte";
